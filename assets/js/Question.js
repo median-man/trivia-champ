da1be4838f7054e401f761b64abaeba91c8e7855
John Desrosiers <johndrunner@yahoo.com>
2018-03-25 09:21:11 -0700

    Add question.render and tests (in progress)

--- assets/js/Question.js
+++ assets/js/Question.js
@@ -2,6 +2,22 @@ class Question {
   constructor(data) {
     this.data = data;
   }
+
+  render() {
+    if (!this.parent) {
+      throw new Error('question.render called before setting parent');
+    }
+    const root = $('<div>');
+    root.text(this.data.question);
+    this.setRootNode(root[0]);
+    return this;
+  }
+
+  setRootNode(element) {
+    this.rootNode = element;
+    $(this.parent).append(this.rootNode);
+  }
+
   setParent(selector) {
     const parent = document.querySelector(selector);
     if (!parent) {
--- tests/js/test.Question.js
+++ tests/js/test.Question.js
@@ -11,6 +11,14 @@ QUnit.module('Question', () => {
       'Vincent van Gogh',
     ],
   };
+
+  const setupNewQuestion = (id) => {
+    $('<div>', { id }).appendTo('#qunit-fixture');
+    const question = new Question(questionData);
+    const parentSelector = `#${id}`;
+    return { parentId: id, parentSelector, question };
+  };
+
   test('Question is defined', assert => assert.ok(Question));
 
   test('has a data property which is equal to data passed to constructor', (assert) => {
@@ -22,41 +30,78 @@ QUnit.module('Question', () => {
   testRender();
 
   function testSetParent() {
-    let question;
+    let setup;
     const id = 'question-1';
     const setParentHooks = {
       beforeEach() {
-        $('<div>', { id }).appendTo('#qunit-fixture');
-        question = new Question(questionData);
+        setup = setupNewQuestion(id);
       },
     };
     QUnit.module('question.setParent', setParentHooks);
-    test('setParent exists', assert => assert.ok(question.setParent));
+    test('setParent exists', assert => assert.ok(setup.question.setParent));
 
-    test('when selector is "#question-1"', (assert) => {
-      question.setParent(`#${id}`);
+    test(`when selector parameter is "#${id}"`, (assert) => {
+      const { question, parentSelector } = setup;
+      question.setParent(parentSelector);
       assert.ok(question.parent, 'question.parent exists');
       assert.ok(question.parent instanceof Element, 'question.parent is an Element');
     });
 
     test('returns the question', (assert) => {
-      assert.equal(question, question.setParent(`#${id}`));
+      const { question, parentSelector } = setup;
+      assert.equal(question, question.setParent(parentSelector));
     });
 
     test('throws if selector does not match an element in the DOM', (assert) => {
       const badSelector = '#nothing';
       const errMatcher = RegExp(`invalid parent selector: ${badSelector}`, 'ig').unicode;
-      assert.throws(() => question.setParent('#nothing'), errMatcher, 'throws with message');
+      const shouldThrow = () => setup.question.setParent(badSelector);
+      assert.throws(shouldThrow, errMatcher, 'throws with message');
     });
   }
 
   function testRender() {
-    QUnit.module.todo('question.render');
-    QUnit.todo('it exists', () => {});
-    QUnit.todo('throws if parent is not defined', () => {});
-    QUnit.todo('returns question', () => {});
-    QUnit.todo('sets rootNode property', () => {});
-    QUnit.todo('append rootNode to parent', () => {});
-    QUnit.todo('renders html at rootNode', () => {});
+    let question;
+    let parentSelector;
+    const hooks = {
+      beforeEach() {
+        ({ question, parentSelector } = setupNewQuestion('test-question'));
+        question.setParent(parentSelector);
+      },
+    };
+    QUnit.module('question.render', hooks);
+    test('it exists', assert => assert.ok(question.render));
+
+    test('throws if parent is not defined', (assert) => {
+      question.parent = undefined;
+      assert.notOk(question.parent, 'when parent is not set');
+
+      const errMatcher = /question.render called before setting parent/gi;
+      assert.throws(question.render, errMatcher, 'throws with message');
+    });
+
+    test('is chainable', (assert) => {
+      assert.equal(question.render(), question, 'returns question instance');
+    });
+
+    test('sets rootNode property', (assert) => {
+      question.render();
+      assert.ok(question.rootNode, 'rootNode property exists');
+      assert.ok(question.rootNode instanceof HTMLElement, 'rootNode is an instance of HTMLElement');
+    });
+
+    test('append rootNode to parent', (assert) => {
+      question.render();
+      const parentHasRootNode = $(question.parent).children()[0] === question.rootNode;
+      assert.equal($(question.parent).children().length, 1, 'parent has only one child');
+      assert.ok(parentHasRootNode, 'rootNode is a child element of parent');
+    });
+
+    test('renders html at rootNode', (assert) => {
+      const questionText = questionData.question;
+      question.render();
+      const html = $(question.rootNode).html();
+      assert.includes(html, question.data.question, 'html includes question text');
+    });
   }
 });
