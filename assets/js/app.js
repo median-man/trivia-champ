3e056ff168d152b0d25d3ac694599bedfe46417c
John Desrosiers <johndrunner@yahoo.com>
2018-03-29 05:53:28 -0700

    Create questionData array in app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,120 @@
-
+const questionData = [
+  {
+    category: 'Art',
+    type: 'multiple',
+    difficulty: 'hard',
+    question: 'Painter Piet Mondrian (1872 - 1944) was a part of what movement?',
+    correct_answer: 'Neoplasticism',
+    incorrect_answers: [
+      'Precisionism',
+      'Cubism',
+      'Impressionism',
+    ],
+  },
+  {
+    category: 'Art',
+    type: 'multiple',
+    difficulty: 'medium',
+    question: 'Which time signature is commonly known as &ldquo;Cut Time?&rdquo;',
+    correct_answer: '2/2',
+    incorrect_answers: [
+      '4/4',
+      '6/8',
+      '3/4',
+    ],
+  },
+  {
+    category: 'Art',
+    type: 'multiple',
+    difficulty: 'medium',
+    question: 'Which artist&rsquo;s studio was known as &#039;The Factory&#039;?',
+    correct_answer: 'Andy Warhol',
+    incorrect_answers: [
+      'Roy Lichtenstein',
+      'David Hockney',
+      'Peter Blake',
+    ],
+  },
+  {
+    category: 'Art',
+    type: 'multiple',
+    difficulty: 'medium',
+    question: 'What nationality was the surrealist painter Salvador Dali?',
+    correct_answer: 'Spanish',
+    incorrect_answers: [
+      'Italian',
+      'French',
+      'Portuguese',
+    ],
+  },
+  {
+    category: 'Art',
+    type: 'multiple',
+    difficulty: 'hard',
+    question: 'What year did Albrecht D&uuml;rer create the painting &quot;The Young Hare&quot;?',
+    correct_answer: '1502',
+    incorrect_answers: [
+      '1702',
+      '1402',
+      '1602',
+    ],
+  },
+  {
+    category: 'Art',
+    type: 'multiple',
+    difficulty: 'hard',
+    question: 'What year was the Mona Lisa finished?',
+    correct_answer: '1504',
+    incorrect_answers: [
+      '1487',
+      '1523',
+      '1511',
+    ],
+  },
+  {
+    category: 'Art',
+    type: 'boolean',
+    difficulty: 'medium',
+    question: 'Pablo Picasso is one of the founding fathers of &quot;Cubism.&quot;',
+    correct_answer: 'True',
+    incorrect_answers: [
+      'False',
+    ],
+  },
+  {
+    category: 'Art',
+    type: 'multiple',
+    difficulty: 'medium',
+    question: 'Which one of these paintings is not by Caspar David Friedrich?',
+    correct_answer: 'The Black Sea',
+    incorrect_answers: [
+      'The Sea of Ice',
+      'Wanderer above the Sea of Fog',
+      'The Monk by the Sea',
+    ],
+  },
+  {
+    category: 'Art',
+    type: 'multiple',
+    difficulty: 'medium',
+    question: 'Which artist&#039;s style was to use small different colored dots to create a picture?',
+    correct_answer: 'Georges Seurat',
+    incorrect_answers: [
+      'Paul C&eacute;zanne',
+      'Vincent Van Gogh',
+      'Henri Rousseau',
+    ],
+  },
+  {
+    category: 'Art',
+    type: 'multiple',
+    difficulty: 'hard',
+    question: 'How many paint and pastel versions of &quot;The Scream&quot; is Norwegian painter Edvard Munch believed to have produced?',
+    correct_answer: '4',
+    incorrect_answers: [
+      '1',
+      '3',
+      '2',
+    ],
+  },
+];
--- tests/js/test.app.js
+++ tests/js/test.app.js
@@ -1,1 +1,9 @@
-/* global  */
+/* global questionData */
+QUnit.module('#01 - Question Data', () => {
+  test('questionData is an array', (assert) => {
+    assert.ok(Array.isArray(questionData), 'Array.isArray(questionData)');
+  });
+  test('questionData has 10 items', (assert) => {
+    assert.equal(questionData.length, 10, 'questionData.length = 10');
+  });
+});
