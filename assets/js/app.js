82a1c6090d51be6c9c819348b8dbfcbc38554294
John Desrosiers <johndrunner@yahoo.com>
2018-03-29 17:25:00 -0700

    Add getOptions and getQuestions methods to question.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,7 @@
 function makeQuestion(questionData) { //eslint-disable-line
+  const options = [questionData.correct_answer, ...questionData.incorrect_answers];
   return {
+    getOptions: () => options,
     getQuestion: () => questionData.question,
   };
 }
--- tests/js/test.app.js
+++ tests/js/test.app.js
@@ -1,5 +1,5 @@
 /* global questionData, makeQuestion */
-QUnit.module('Question Data (#01)', () => {
+QUnit.module('Question Data', () => {
   test('questionData is an array', (assert) => {
     assert.ok(Array.isArray(questionData), 'Array.isArray(questionData)');
   });
@@ -8,7 +8,7 @@ QUnit.module('Question Data (#01)', () => {
   });
 });
 
-QUnit.module('question model (#01)', () => {
+QUnit.module('question model', () => {
   const openTDBQuestions = [{
     category: 'Art',
     type: 'multiple',
@@ -35,17 +35,56 @@ QUnit.module('question model (#01)', () => {
   test('makeQuestion exists', (assert) => {
     assert.ok(makeQuestion);
   });
+
+  test('has expected methods', (assert) => {
+    const question = makeQuestion(openTDBQuestions[0]);
+    const expectedMethods = ['getQuestion', 'getOptions'];
+    expectedMethods.forEach(hasMethod);
+
+    function hasMethod(methodName) {
+      assert.ok(question[methodName], `${methodName} exists`);
+      assert.equal(typeof question[methodName], 'function', `${methodName} is a function`);
+    }
+  });
+
   QUnit.module('getQuestion', () => {
-    test('getQuestion exists', (assert) => {
-      const question = makeQuestion(openTDBQuestions[0]);
-      assert.ok(question.getQuestion, 'question.getQuestion exists');
-    });
     openTDBQuestions.forEach(testGetQuestion);
+
     function testGetQuestion(testData) {
       const questionText = testData.question;
       test(`When questionData.question is "${questionText}"`, (assert) => {
         const question = makeQuestion(testData);
         assert.equal(question.getQuestion(), questionText, `getQuestion() returns "${questionText}`);
+      });
+    }
+  });
+
+  QUnit.module('getOptions', () => {
+    test('returns an array', (assert) => {
+      const question = makeQuestion(openTDBQuestions[0]);
+      assert.ok(Array.isArray(question.getOptions()));
+    });
+
+    openTDBQuestions.forEach(testOptions);
+    function testOptions(testData) {
+      const question = makeQuestion(testData);
+      const options = question.getOptions();
+      testCorrectAnswer(testData, options);
+      testIncorrectAnswers(testData, options);
+    }
+
+    function testCorrectAnswer(testData, options) {
+      const expected = testData.correct_answer;
+      test(`When the questionData.correct_answer is ${expected}`, (assert) => {
+        assert.includes(options, expected);
+      });
+    }
+
+    function testIncorrectAnswers(testData, options) {
+      const expected = testData.incorrect_answers;
+      const expectedString = expected.join(', ');
+      test(`When questionData.incorrect_answers is ${expectedString}`, (assert) => {
+        assert.includes(options, expected, `returned array includes ${expectedString}`);
       });
     }
   });
